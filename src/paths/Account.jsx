91b1bf75ec2d6f507bd5f31dac5896191cadb31d
Lea Abeyta <lea.abeyta@gmail.com>
2017-12-29 12:05:16 -0700

    updating and deleting events, tablehosts, and guests

--- src/paths/Account.jsx
+++ src/paths/Account.jsx
@@ -25,14 +25,14 @@ class Account extends Component {
   render() {
     return(
   <div>
-    <div id="ms-preload" className="ms-preload">
+    {/*<div id="ms-preload" className="ms-preload">
       <div id="status">
         <div className="spinner">
           <div className="dot1"></div>
           <div className="dot2"></div>
         </div>
       </div>
-    </div>
+    </div>*/}
     <div className="ms-site-container">
 
     <Navbar />
