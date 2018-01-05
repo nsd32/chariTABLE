a0b758480eff49cc194b575493af56cad1c7bda5
Erik Streff <Estreff@gmail.com>
2018-01-05 13:28:25 -0700

    trying hard code style in Guest Register

--- client/src/paths/GuestRegister.jsx
+++ client/src/paths/GuestRegister.jsx
@@ -51,11 +51,11 @@ class GuestRegister extends Component {
   render() {
     if(!this.state.tableHostData) {
       return (
-        <div id="ms-preload" class="ms-preload">
+        <div id="ms-preload" className="ms-preload">
           <div id="status">
-            <div class="spinner">
-              <div class="dot1"></div>
-              <div class="dot2"></div>
+            <div className="spinner">
+              <div className="dot1"></div>
+              <div className="dot2"></div>
             </div>
           </div>
         </div>
@@ -64,46 +64,46 @@ class GuestRegister extends Component {
     return(
       <div>
 
-        {/*<div id="ms-preload" class="ms-preload">
+        {/*<div id="ms-preload" className="ms-preload">
           <div id="status">
-            <div class="spinner">
-              <div class="dot1"></div>
-              <div class="dot2"></div>
+            <div className="spinner">
+              <div className="dot1"></div>
+              <div className="dot2"></div>
             </div>
           </div>
         </div>*/}
-        <div class="bg-full-page ms-hero-img-coffee ms-hero-bg-primary ms-bg-fixed back-fixed">
+        <div className="bg-full-page ms-hero-img-coffee ms-hero-bg-primary ms-bg-fixed back-fixed">
 
-          <header class="ms-header ms-header-primary">
-            <div class="container container-full">
-              <div class="ms-title">
+          <header className="ms-header ms-header-primary">
+            <div className="container container-full">
+              <div className="ms-title">
                 <a href="index.html">
 
-                  <span class="ms-logo animated zoomInDown animation-delay-5">CH</span>
-                  <h1 class="animated fadeInRight animation-delay-6">chariTABLE Host
+                  <span className="ms-logo animated zoomInDown animation-delay-5">CH</span>
+                  <h1 className="animated fadeInRight animation-delay-6">chariTABLE Host
                   </h1>
                 </a>
               </div>
-              <div class="header-right">
+              <div className="header-right">
               </div>
             </div>
           </header>
 
-          <div class="absolute-center">
-            <div class="container">
-              <div class="row">
-                <div class="col-xl-6">
-                  <div class="card card-flat bg-transparent">
-                    <div class="card-block color-white">
-                      <header class="text-center mb-2">
-                        <h1 class="no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5"><span>{this.state.tableHostData.searchedEvent.eventName}</span>
+          <div className="absolute-center">
+            <div className="container">
+              <div className="row">
+                <div className="col-xl-6">
+                  <div className="card card-flat bg-transparent">
+                    <div className="card-block color-white">
+                      <header className="text-center mb-2">
+                        <h1 className="no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5"><span>{this.state.tableHostData.searchedEvent.eventName}</span>
                         </h1>
-                        <h1 class="no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5">Table Host: {this.state.tableHostData.tableHost.name}
+                        <h1 className="no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5">Table Host: {this.state.tableHostData.tableHost.name}
                         </h1>
                       </header>
-                      <div class="row">
-                        <div class="col-12">
-                          <div class="text-center card-block animated zoomIn animation-delay-10">
+                      <div className="row">
+                        <div className="col-12">
+                          <div className="text-center card-block animated zoomIn animation-delay-10">
                             <h2 className="animated zoomInDown animation-delay-5">Guests</h2>
                             <ul id="guest-list">
                               {this.state.tableHostData.tableHost.guests.map((guest, idx) => {
@@ -114,8 +114,8 @@ class GuestRegister extends Component {
                             </ul>
                           </div>
                         </div>
-                        {/*<div class="col-sm-6">
-                          <div class="text-center card-block animated zoomIn animation-delay-10">
+                        {/*<div className="col-sm-6">
+                          <div className="text-center card-block animated zoomIn animation-delay-10">
                             <h2 className="animated zoomInDown animation-delay-5">Sponsors</h2>
                             <ul>
                               {this.state.tableHostData.searchedEvent.sponsors.map((sponsor, idx) => {
@@ -130,34 +130,34 @@ class GuestRegister extends Component {
                     </div>
                   </div>
                 </div>
-                <div class="col-xl-6">
-                  <div class="card index-1 animated zoomInRight animation-delay-7">
-                    <div class="card-block-big">
-                      <h1 class="color-primary">Register</h1>
+                <div className="col-xl-6">
+                  <div className="card index-1 animated zoomInRight animation-delay-7">
+                    <div className="card-block-big">
+                      <h1 className="color-primary">Register</h1>
                       <hr style={{height:"10px", visibility:"hidden"}} />
                       <form>
-                        <div class="form-group label-floating mt-2 mb-1">
-                          <div class="input-group center-block">
-                            <label class="control-label" for="ms-subscribe">
-                              <i class="zmdi zmdi-email"></i> Full Name</label>
-                            <input name="guestName" onChange={this.handleInputChange} type="text" id="ms-subscribe" class="form-control" /> </div>
+                        <div className="form-group label-floating mt-2 mb-1">
+                          <div className="input-group center-block">
+                            <label className="control-label" htmlFor="ms-subscribe">
+                              <i className="zmdi zmdi-email"></i> Full Name</label>
+                            <input name="guestName" onChange={this.handleInputChange} type="text" id="ms-subscribe" className="form-control" /> </div>
                         </div>
-                        <div class="form-group label-floating mt-2 mb-1">
-                          <div class="input-group center-block">
-                            <label class="control-label" for="ms-subscribe">
-                              <i class="zmdi zmdi-email"></i> Email Address</label>
-                            <input name="guestEmail" onChange={this.handleInputChange} validate type="email" id="ms-subscribe" class="form-control" /> </div>
+                        <div className="form-group label-floating mt-2 mb-1">
+                          <div className="input-group center-block">
+                            <label className="control-label" htmlFor="ms-subscribe">
+                              <i className="zmdi zmdi-email"></i> Email Address</label>
+                            <input name="guestEmail" onChange={this.handleInputChange} type="email" id="ms-subscribe" className="form-control" /> </div>
                         </div>
-                        <button onClick={this.registerButton} class="btn btn-raised btn-primary btn-block" type="button">Register</button>
+                        <button onClick={this.registerButton} className="btn btn-raised btn-primary btn-block" type="button">Register</button>
                         <h3>Can't make it?</h3>
                         <p>Enter your name above and click Donate to help {this.state.tableHostData.tableHost.name} meet their goal.</p>
-                        <button class="btn btn-raised btn-success btn-block" type="button">Donate</button>
+                        <button className="btn btn-raised btn-success btn-block" type="button">Donate</button>
                       </form>
                     </div>
                   </div>
                 </div>
               </div>
-              <p class="lead lead-sm text-center mt-4 color-medium animated fadeInUp animation-delay-16">Jubatus Software &copy; Copyright 2017</p>
+              <p className="lead lead-sm text-center mt-4 color-medium animated fadeInUp animation-delay-16">Jubatus Software &copy; Copyright 2017</p>
             </div>
           </div>
         </div>
